be4fbdc40354f9fcb56881f34ad7d8bd713e418d
Gustavo <114709276+GusMuza@users.noreply.github.com>
2022-12-19 01:38:44 -0300

    Update consultas_supermark.py

--- Consultas/consultas_supermark.py
+++ Consultas/consultas_supermark.py
@@ -1,6 +1,7 @@
 import sqlite3
 from sqlite3 import Error
 
+
 def create_connection(db_file):
     """ create a database connection to the SQLite database
         specified by the db_file
@@ -32,6 +33,12 @@ def select_userid(conn,email):
     row = cur.fetchone()
     return row
 
+def select_tipoid(conn,tipo):
+    cur = conn.cursor()
+    cur.execute("SELECT tipoId FROM tipos WHERE descripcion= ?",tipo)
+    row = cur.fetchone()
+    return row
+
 def select_descuento_porcentaje(conn):
     """
     Query all rows in the tasks table
@@ -43,6 +50,7 @@ def select_descuento_porcentaje(conn):
     rows = cur.fetchone()
     # print(rows)
     return rows
+
 
 def update_descuento(conn, descuento):
     """
@@ -95,17 +103,39 @@ def select_comprobanteId(conn):
     # print(rows)
     return rows
 
+def select_all_comprobantes(conn):
+    """
+    Query all rows in the tasks table
+    :param conn: the Connection object
+    :return:
+    """
+    cur = conn.cursor()
+    cur.execute("SELECT 'F'||CO.tipo||' - '||CO.comprobanteId, CO.fecha, US.email, PR.codigo || ' - ' ||PR.nombre, DE.cantidad, DE.precio, CO.total FROM comprobantes AS CO INNER JOIN usuarios AS US ON US.usuarioId =co.usuarioId INNER JOIN detalles AS DE ON de.comprobanteId = co.comprobanteId INNER JOIN productos AS PR ON DE.productoId = PR.productoId ")
+
+    rows = cur.fetchall()
+    # print(rows)
+    return rows    
+
+def consulta_stock(conn,id):
+    cur = conn.cursor()
+    cur.execute("SELECT stock FROM productos WHERE productoId= ?",id)
+    row = cur.fetchone()
+    return row
+
+
 def main():
     database = r"Supermark.db"
     # create a database connection
     conn = create_connection(database) 
     with conn:
-        print("Mostrar descuentos")
-        print(select_all_descuentos(conn))
-        print(select_all_tarjetas(conn,"5"))
-        print("Mostrar id de usuario según email")
-        print(select_userid(conn,("abelu89@gmail.com",)))
-        print(select_comprobanteId(conn))       
+        #print("Mostrar descuentos")
+        #print(select_all_descuentos(conn))
+        #print(select_all_tarjetas(conn,"5"))
+        #print("Mostrar id de usuario según email")
+        #print(select_userid(conn,("abelu89@gmail.com",)))
+        #print(select_comprobanteId(conn))       
+        #print(select_all_comprobantes(conn))
+        print(consulta_stock(conn,"1"))
 
 if __name__ == '__main__':
     main()
